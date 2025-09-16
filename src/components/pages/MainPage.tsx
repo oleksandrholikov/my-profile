969f0d465b5a80682f21c4e8d1c33f1f00f57113
HOLIKOV Oleksandr <oleksandr.holikov@epitech.eu>
2025-09-16 16:49:31 +0200

    feat: create React+Vite+TS app

---
+++ src/components/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import IconVScode from '@/assets/icons/icon-vscode.png'
+
+export default function MainPage(){
+    const [isActve, setIsActive] = useState<string>('');
+
+
+
+    return(
+        <>
+        </>
+    )
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@elements': path.resolve(__dirname, './src/elements'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@types': path.resolve(__dirname, './src/types')      
+    },
+  },
+})
