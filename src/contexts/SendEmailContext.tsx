afed1f1b9caade677e58141784d8f4ed7f9af6f4
HOLIKOV Oleksandr <oleksandr.holikov@epitech.eu>
2025-09-30 17:27:36 +0200

    fix: rewrite section abour, add section expiriens

--- src/contexts/SendEmailContext.tsx
+++ src/contexts/SendEmailContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, useContext, useEffect, useState} from 'react';
+import {createContext, useContext, useState} from 'react';
 import type {ReactNode} from 'react'
 
 type SendEmailFormContextType = {
